5ded3bb75ad5c694240c27d2a186898bdd9aa43c
SirShine <50844675+SirShine@users.noreply.github.com>
2021-07-13 01:32:55 +0200

    Demonfall Item Autofarm Update

--- Demonfall/Demonfall Item Autofarm Release.lua
+++ Demonfall/Demonfall Item Autofarm Release.lua
@@ -1,4 +1,5 @@
 getgenv().enabled = true;
 getgenv().speed = 75;
+getgenv().offset = Vector3.new(0, -3, 0);
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SirShine/Obfuscated-Roblox-Scripts/main/Demonfall/Demonfall%20Item%20Autofarm.lua", true))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SirShine/Obfuscated-Roblox-Scripts/main/Demonfall/Item%20Autofarm.lua", true))();
---
+++ Demonfall/Item Autofarm.lua
@@ -0,0 +1,45 @@
+local function getClosestItem()
+    local closestItem = nil;
+    local closestItemDistance = math.huge;
+    for _, v in pairs(game:GetService("Workspace"):GetChildren()) do
+        if(v:FindFirstChild("Part") and v:FindFirstChild("Spawn")  and v.Name ~= "Trinkets") then
+            local distance = game:GetService("Players").LocalPlayer:DistanceFromCharacter(v.Part.Position);
+            if(distance < closestItemDistance) then
+                closestItem = v;
+                closestItemDistance = distance;
+            end
+        end
+    end
+    return {closestItem, closestItemDistance};
+end
+
+game:GetService('RunService').Stepped:connect(function()
+    if(getgenv().enabled == true) then
+	    game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(11);
+    end
+end)
+
+coroutine.wrap(function()
+
+    local Tween = nil;
+    while getgenv().enabled == true do
+        wait(1)
+        local closestItem = getClosestItem();
+        local item = closestItem[1];
+        local distance = closestItem[2];
+    
+        Tween = game:GetService("TweenService"):Create(
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,
+            TweenInfo.new(distance/getgenv().speed, Enum.EasingStyle.Linear),
+            {Position = item.Part.Position + getgenv().offset}
+        );
+    
+        Tween:Play();
+        Tween.Completed:Wait();
+    
+        keypress(0x45);
+        wait(math.random(100, 500)/1000);
+        keyrelease(0x45);
+    end
+
+end)()
